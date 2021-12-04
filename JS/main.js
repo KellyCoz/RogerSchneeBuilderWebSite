df0c60dcf707937dfd53e3292fa35a1a98ed4ea9
KellyCoz <kelly.cozmanciuc@gmail.com>
2021-12-03 21:47:20 -0500

    Latest working copy

--- JS/main.js
+++ JS/main.js
@@ -3,37 +3,3 @@ function toggleMenu() {
 }
 let x = document.getElementById('hamburgerNav');
 x.onclick = toggleMenu;
-
-/*"use strict";
-function validateForm(){
-    var status = true;
-
-    varx;
-
-    x = document.forms.estimate.name.value;
-    console.log(x);
-
-    return status;
-}*/
-
-function createThanks(){
-
-    var n = document.getElementById("name");
-    var e = document.forms.estimate.email.value;
-    var p = document.forms.estimate.phone.value;
-    var contact;
-    
-    if(e!="" && p!=""){
-        contact = e +" and/or "+p;
-    }
-    else if(e!=""){
-        contact = e;
-    }
-    else if(p!=""){
-        contact = p;
-    }
-
-    var message = "Thank you! "+n+" will be contacted at "+contact+" within two business days.";
-console.log(message);
-    return message;
-}
